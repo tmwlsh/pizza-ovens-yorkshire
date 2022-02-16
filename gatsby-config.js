8a11f9f67ebc68c3bde1d6a33041bab1ca42677c
TMWLSH <hello@tmwlsh.co.uk>
2022-02-16 17:39:01 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+      title: `Pizza Ovens Yorkshire`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"]
+};
