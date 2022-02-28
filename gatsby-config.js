358453b324f776f2d4b6237ca8cc521766abbef7
TMWLSH <hello@tmwlsh.co.uk>
2022-02-28 17:39:25 +0000

    initial staging commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
       title: `Pizza Ovens Yorkshire`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"]
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /svgs/ // See below to configure properly
+        }
+      }
+    }
+  ]
 };
