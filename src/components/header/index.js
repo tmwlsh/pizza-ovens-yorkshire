d080a2d76c09ed5ea6cc18c59be1bcf1bb7eb5a8
TMWLSH <hello@tmwlsh.co.uk>
2022-02-28 18:24:00 +0000

    feat: responsive header done

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,27 @@
 import * as React from "react";
+import { Link } from "gatsby";
 import * as styles from "./styles.module.scss";
 
 import { Container } from "../container";
+
+import Logo from "../../svgs/logo.svg";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <Container size="large">
-        <h1>Header</h1>
+        <div className={styles.headerInner}>
+          <Logo />
+          <ul>
+            <li><Link to="/">About</Link></li>
+            <li><Link to="/">Rentals</Link></li>
+            <li><Link to="/">Testimonials</Link></li>
+            <li><Link to="/">Blog</Link></li>
+          </ul>
+          <ul>
+            <li><Link to="/">Contact</Link></li>
+          </ul>
+        </div>
       </Container>
     </header>
   )
